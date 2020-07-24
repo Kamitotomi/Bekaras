5649c32454df97555dce45adfae691963765de40
Kamitotomi <rockyaaokage@gmail.com>
2020-07-24 12:34:53 +0900

    Mypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  
   root 'home#top'
   get 'home/about' => 'home#about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:new, :create, :show, :destroy]
+
+  resources :users, only: [:show, :index, :edit, :update]
+
 end
