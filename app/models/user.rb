301394113e3707781523fb42a68e05510660e472
Kamitotomi <rockyaaokage@gmail.com>
2020-07-28 00:42:31 +0900

    Addfavorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   #　フォロー機能
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy # フォロー取得
